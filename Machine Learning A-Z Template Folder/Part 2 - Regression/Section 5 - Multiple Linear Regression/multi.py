a10a330be4cf3ea27db536cb8285d2a31983020d
tony4eyes <41983299+tony4eyes@users.noreply.github.com>
2018-08-01 23:03:39 -0700

    ML

--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 5 - Multiple Linear Regression/multi.py
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 5 - Multiple Linear Regression/multi.py
@@ -1,1 +1,16 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
 
+dataset = pd.read_csv('50_Startups.csv')
+
+X = dataset.iloc[:,:-1].values
+Y = dataset.iloc[:,4].values
+
+import sklearn.preprocessing as pc
+
+le = pc.LabelEncoder();
+X[:,3] = le.fit_transform(X[:,3])
+
+#OneHotEncoder - categorical integer
+oe = pc.OneHotEncoder(categorical_features = [3]);
+X = oe.fit_transform(X).toarray()
