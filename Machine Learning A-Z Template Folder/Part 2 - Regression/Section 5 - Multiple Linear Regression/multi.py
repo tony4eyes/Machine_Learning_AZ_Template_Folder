e87147f0edb5ee00484664215cd887b6f812a76a
tony4eyes <41983299+tony4eyes@users.noreply.github.com>
2018-08-01 23:15:55 -0700

    ML_2

--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 5 - Multiple Linear Regression/multi.py
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 5 - Multiple Linear Regression/multi.py
@@ -8,9 +8,20 @@ Y = dataset.iloc[:,4].values
 
 import sklearn.preprocessing as pc
 
+#LabelEncoder - convert categories to categorical integer
 le = pc.LabelEncoder();
 X[:,3] = le.fit_transform(X[:,3])
 
 #OneHotEncoder - categorical integer
 oe = pc.OneHotEncoder(categorical_features = [3]);
 X = oe.fit_transform(X).toarray()
+
+import sklearn.model_selection as cv
+X_train, X_test, Y_train, Y_test =  cv.train_test_split(X, Y, test_size = 0.2, random_state = 0)
+
+import sklearn.linear_model as lm
+regressor = lm.LinearRegression()
+regressor.fit(X_train, Y_train)
+regressor.predict(X_test)
+
+import matplotlib.pyplot as plt
